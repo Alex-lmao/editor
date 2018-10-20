2e8f9ee8cd9113ed58b717ec886bf71b6d0849b7
Alexander Norozkhani <aleno523@student.liu.se>
2018-10-20 19:25:43 +0200

    removed useless files

--- editor/test.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-require 'gtk3'
-# require 'libglade2' #you don't need this anymore
-
-builder = Gtk::Builder.new
-builder.add_from_file("~/Documents/programming_challenges/editor/builder.ui")
-builder.connect_signals {|handler| method(handler) }
